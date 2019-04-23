3d498379a575f78742d0e818fc32f8f4ce9fdd4a
Kyle Huang <kyle.huang168@gmail.com>
2019-04-23 23:57:53 +0800

    4/23

---
+++ 167. Two Sum II - Input array is sorted/TwoSumV2.java
@@ -0,0 +1,29 @@
+public int[] twoSum(int[] num, int target) {
+    
+    int[] ans = new int[2];
+
+    if (num == null || num.length < 2) 
+        return ans;
+
+    int left = 0;
+    int right = num.length - 1;
+
+    while (left < right) {
+        int sum = num[left] + num[right];
+        if (sum == target) 
+        {
+            ans[0] = left + 1;
+            ans[1] = right + 1;
+            break;
+        } 
+        else if (sum > target) 
+        {
+            --right;
+        } 
+        else 
+        {
+            ++left;
+        }
+    }
+    return ans;
+}
